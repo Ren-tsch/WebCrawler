d405f591573964608b8a76a595da28844819d217
Rentsch <rentsch_s@hotmail.com>
2022-12-23 08:58:42 +0100

    Benutzereingabe URL eingefügt

--- WebCrawler/Program.cs
+++ WebCrawler/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace WebCrawler
+﻿using HtmlAgilityPack;
+using System.Diagnostics;
+using System.Net;
+
+namespace WebCrawler
 {
     internal class Program
     {
         static void Main(string[] args)
+        
         {
-            Console.WriteLine("Hello, World!");
-        }
+            //Abfrage von Webseiten-URL
+            Console.WriteLine("URL eingeben. Bsp. https://www.google.com");
+            string url = Console.ReadLine();
+
+            //Herunterladen der html Datei und abspeichern bei angegebenem Pfad
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = client.GetAsync(url).Result)
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        string result = content.ReadAsStringAsync().Result;
+                        File.WriteAllText("C:/Users/Rentsch/Desktop/test.html", result);
+                    }
+                }
+            }
+        }   
     }
 }
